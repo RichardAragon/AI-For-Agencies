730bde089f0aadb8b85333250388f8c095006382
RichardAragon <138004861+RichardAragon@users.noreply.github.com>
2024-08-07 13:08:06 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import './App.css';
-import Services from './components/Services';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>AI For Agencies</h1>
+        <h1>Welcome to My React App</h1>
       </header>
-      <main>
-        <Services />
-      </main>
     </div>
   );
 }
 
 export default App;
+
