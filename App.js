f27174670f1a77b0f66cbe6895180a697c45488a
RichardAragon <138004861+RichardAragon@users.noreply.github.com>
2024-08-07 12:43:48 -0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Services from './components/Services';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>AI For Agencies</h1>
+      </header>
+      <main>
+        <Services />
+      </main>
+    </div>
+  );
+}
+
+export default App;
